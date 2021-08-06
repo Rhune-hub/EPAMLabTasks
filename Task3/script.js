b4fdcc4c073eb4676dfc75a8dcb5bf21da42fb7b
Rhune <i.kaminsky@mail.ru>
2021-08-07 02:21:09 +0300

    Add new function and fix some bad practice code.

--- Task3/script.js
+++ Task3/script.js
@@ -8,59 +8,100 @@ const secondObjectExample = {
     z: 'v',
 };
 
+const resultTextarea = document.getElementById('result-object');
+const objectsSet = document.getElementsByName('objects-set');
 
-main();
+init();
 
-function main() {
-    let textareas = document.getElementsByName('objects-set');
-    textareas.forEach(ta => ta.addEventListener('focusin',textareaHandler));
-    document.getElementById('add-objects').addEventListener('click',interactObjects);
-    console.log(textareas);
-    textareas[0].value = JSON.stringify(firstObjectExample);
-    textareas[1].value = JSON.stringify(secondObjectExample);
+function init() {
+    addObjectBlock(1);
+    addObjectBlock(2);
+    objectsSet[0].value = JSON.stringify(firstObjectExample);
+    objectsSet[1].value = JSON.stringify(secondObjectExample); 
+    document.getElementById('add-objects')
+            .addEventListener('click', addObjectsHandler);
+    document.getElementById('intersect-objects')
+            .addEventListener('click',intersectObjectsHandler);
 }
 
-function getObject(JSONObj) {
+function addObjectsHandler() {
+    const objects = getObjectsSet();
+    const resultedObject = addObjects(...objects);
+    resultTextarea.value = JSON.stringify(resultedObject);
+}
+
+function intersectObjectsHandler() {
+    const objects = getObjectsSet();
+    const resultedObject = intersectObjects(...objects);
+    resultTextarea.value = JSON.stringify(resultedObject);
+}
+
+function parseJSON(JSONObj) {
     let result = {};
     try {
-       result = JSON.parse(JSONObj);
-    } catch (e) {}
+        result = JSON.parse(JSONObj);
+    } catch (e) {
+        result = undefined;
+    }
     return result;
 }
 
-function isJSON(str) {
-    try {
-       result = JSON.parse(str);
-    } catch (e) {
-        return false;
-    }
-    return true;
-}
-
-function interactObjects() {
-    const rowObjects = Array.from(document.getElementsByName('objects-set'));
-    const objects = rowObjects.map(obj => getObject(obj.value))
-        .filter(obj => Object.getOwnPropertyNames(obj).length > 0);
-    
-    const resultedObject = addObjects(...objects);
-    document.getElementById('result-object').value = JSON.stringify(resultedObject);
+function getObjectsSet() {
+    const rowObjects = Array.from(objectsSet);
+    const objects = rowObjects.map(obj => parseJSON(obj.value))
+        .filter(obj => obj && Object.getOwnPropertyNames(obj).length > 0);
+    return objects;
 }
 
 function addObjects(...objects) {
-    const resultedObject = {};
-    objects.forEach(o => {
-        addObjectsEntries(resultedObject, o);
-    });
+    let resultedObject = {};
+    for(let o of objects) {
+        resultedObject = addObjectsEntries(resultedObject, o);
+    console.log(resultedObject);
+    }
 
     return resultedObject;
 }
+
 function addObjectsEntries(obj1, obj2) {
+    let upgradedObject = {...obj1};
     Object.entries(obj2).forEach(([prop, value]) => {
         if (prop in obj1)
-            obj1[prop] += value;
+            upgradedObject = {
+                ...upgradedObject,
+                [prop]: upgradedObject[prop] + value,
+            };
         else
-            obj1[prop] = value;
+            upgradedObject = {
+                ...upgradedObject,
+                    [prop]: value,
+            };
     });
+        
+    return upgradedObject;
+}
+    
+function intersectObjects(obj1, ...objects) {
+    let resultedObject = {};
+    const obj1Props = Object.entries(obj1);
+    for(let [prop, value] of obj1Props) {
+        let isEverywhere = true;
+        let propValueAccumulator = value;
+        for(let o of objects) {
+            if(!(prop in o)) {
+                isEverywhere = false;
+                break;
+            }
+            propValueAccumulator += o[prop];
+        }
+        if (!isEverywhere) continue;
+        resultedObject = {
+            ...resultedObject,
+            [prop]: propValueAccumulator,
+        };
+    }
+
+    return resultedObject;
 }
 
 function addObjectBlock(number) {
@@ -75,7 +116,7 @@ function addObjectBlock(number) {
     textarea.classList.add('object-field');
     textarea.name = 'objects-set';
     textarea.id = `object-${number}`;
-    textarea.addEventListener('change',textareaHandler);
+    textarea.addEventListener('input',textareaHandler);
 
     container.appendChild(label);
     container.appendChild(textarea);
@@ -85,12 +126,16 @@ function addObjectBlock(number) {
                         
 }
 
-function textareaHandler() {
+function textareaHandler(event) {
+    const currentTextarea = event.currentTarget;
     const textareas = document.getElementsByName('objects-set');
     const lastTextarea = textareas[textareas.length - 1]; 
-    const curNumber = +(lastTextarea.id.split('-')[1]);
-    if (lastTextarea.value.trim() !== '' && isJSON(lastTextarea.value))
-        addObjectBlock(curNumber+1);
-    else if(lastTextarea.value.trim() === '' && !isJSON(lastTextarea.value))
+    const currentNumber = +(currentTextarea.id.split('-')[1]);
+    const lastNumber = +(lastTextarea.id.split('-')[1]);
+    const lastObject = parseJSON(lastTextarea.value);
+
+    if (lastObject)
+        addObjectBlock(currentNumber+1);
+    else if(!lastObject && currentNumber == lastNumber-1)
         lastTextarea.parentElement.remove();
 }
