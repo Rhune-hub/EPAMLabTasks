4c195794170347d69ea2ec8980fbf0db6bc01076
Rhune <i.kaminsky@mail.ru>
2021-10-21 19:18:50 +0300

    Create Multi Module feature

--- Task14/webpack.config.js
+++ Task14/webpack.config.js
@@ -1,31 +1,3 @@
-// const webpack = require('webpack');
-// const path = require('path');
-
-// const config = {
-//   entry: './index.js',//, './apiFunction.js','./apiCategories.js','./apiEntries.js'],
-//   output: {
-//     path: path.resolve(__dirname, 'bundle'),
-//     filename: 'index.js', //'apiFunction.js', 'apiCategories', 'apiEntries.js']
-//   },
-//   mode: "development",
-//   module: {
-//     rules: [
-//       {
-//         test: /\.m?js$/,
-//         exclude: /(node_modules|bower_components)/,
-//         use: {
-//           loader: 'babel-loader',
-//           options: {
-//             presets: ['@babel/preset-env'],
-//             plugins: ["@babel/plugin-syntax-dynamic-import","@babel/plugin-transform-runtime"]            
-//           }
-//         }
-//       }
-//     ]
-//   }
-// };
-
-// module.exports = config;
 const webpack = require('webpack');
 const path = require('path');
 
@@ -33,9 +5,24 @@ const config = {
   entry: './index.js',
   output: {
     path: path.resolve(__dirname, 'bundle'),
-    filename: 'index.js'
+    filename: 'index.js',
   },
-  mode: 'development'
+  mode: "development",
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+            plugins: ["@babel/plugin-syntax-dynamic-import","@babel/plugin-transform-runtime"]            
+          }
+        }
+      }
+    ]
+  }
 };
 
 module.exports = config;
